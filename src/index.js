5e69cd323037873fdc0ecb804685bc2100cda2f4
Пупышева Елена Александровна (об.) <s215029@s.uni.spmi.ru>
2022-07-19 17:23:39 +0300

    fix: tws solved

--- src/index.js
+++ src/index.js
@@ -2,5 +2,20 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+let j;
+let z=0;
+let arr=[];
+if (matrix != undefined){
+j=matrix.length;
+for (; z<j; z++){
+  if ((z==0) || ((z%2)==0)) {
+      arr[z]=matrix[z];
+      }
+     else if ((z%2)!=0) {
+      arr[z]=matrix[z].reverse();}}
+var result = [].concat(...arr);
+return result;}
+      else {return []}
+
 }
+ 
